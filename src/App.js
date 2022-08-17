3cc95abfb2bc4e01399461872f49d6e896986369
ahmadshahdarwesh <abadkaka@yahoo.com>
2022-08-17 11:22:50 +0200

    some styling done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,36 @@ const api = {
 };
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDay();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return `${day} ${date} ${month}`;
+  };
   return (
     <div className="app">
       <main>
@@ -16,6 +46,10 @@ function App() {
             type="text"
             placeholder="Type city name ...."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Neuss Germany</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
