5704a7c59622bf79270fc94fab32ff9465ee62e4
ahmadshahdarwesh <abadkaka@yahoo.com>
2022-08-17 10:15:29 +0200

    api created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
 const api = {
   key: "2d730ae5df478b06fa385a42f118ec2c",
   url: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <container>
+        <div className="search-box">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Type city name ...."
+          />
+        </div>
+      </container>
+    </div>
+  );
 }
 
 export default App;
