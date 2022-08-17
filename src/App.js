39286c18fd72098fd292b83bcca9364033cdbbee
ahmadshahdarwesh <abadkaka@yahoo.com>
2022-08-17 10:01:05 +0200

    Files created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const api = {
+  key: "2d730ae5df478b06fa385a42f118ec2c",
+  url: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
